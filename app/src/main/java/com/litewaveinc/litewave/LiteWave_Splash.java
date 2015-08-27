e7d07723dc58edf91ff7e624fd4184d6179d39c4
jkruljac <jkruljac@hotmail.com>
2015-08-26 19:18:22 -0700

    Initial Commit

---
+++ app/src/main/java/com/litewaveinc/litewave/LiteWave_Splash.java
@@ -0,0 +1,52 @@
+package com.litewaveinc.litewave;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class LiteWave_Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lite_wave__splash);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally{
+                    Intent myIntent = new Intent(LiteWave_Splash.this, ChooseLevel.class);
+                    startActivity(myIntent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lite_wave__splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
