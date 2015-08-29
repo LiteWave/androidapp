b8927517af553a56d97c05d9780d0be708467271
jkruljac <jkruljac@hotmail.com>
2015-08-28 17:49:29 -0700

    Added RESTClientHelper Refactored

--- app/src/main/java/com/litewaveinc/litewave/LiteWave_Splash.java
+++ app/src/main/java/com/litewaveinc/litewave/LiteWave_Splash.java
@@ -7,18 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class LiteWave_Splash extends AppCompatActivity {
 
     @Override
@@ -34,36 +22,7 @@ public class LiteWave_Splash extends AppCompatActivity {
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally{
-
-                    try {
-                        URL url;
-                        HttpURLConnection conn;
-                        BufferedReader rd;
-                        String line;
-                        StringBuilder result = new StringBuilder();
-
-                        url = new URL("http://104.130.156.82:8080/api/clients/5260316cbf80240000000001/events");
-                        conn = (HttpURLConnection) url.openConnection();
-                        conn.setRequestMethod("GET");
-
-                        rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-                        while ((line = rd.readLine()) != null) {
-                            result.append(line);
-                        }
-                        rd.close();
-                        String responseText = result.toString();
-                        JSONArray mainResponseJSONObject = new JSONArray(responseText);
-                        //mainResponseJSONObject = mainResponseJSONObject;
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    } catch (ProtocolException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    RESTClientHelper.callRESTService(getString(R.string.getEventsURL));
                     Intent myIntent = new Intent(LiteWave_Splash.this, ChooseLevel.class);
                     startActivity(myIntent);
                 }
---
+++ app/src/main/java/com/litewaveinc/litewave/RESTClientHelper.java
@@ -0,0 +1,50 @@
+package com.litewaveinc.litewave;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by jonathan on 8/28/15.
+ */
+public class RESTClientHelper {
+    public static JSONArray callRESTService(String enpointURL) {
+        try {
+            URL url = new URL(enpointURL);
+            HttpURLConnection conn;
+            BufferedReader rd;
+            String line;
+            StringBuilder result = new StringBuilder();
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            String responseText = result.toString();
+            return(new JSONArray(responseText));
+            //mainResponseJSONObject = mainResponseJSONObject;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return null;
+        }
+    }
+}
