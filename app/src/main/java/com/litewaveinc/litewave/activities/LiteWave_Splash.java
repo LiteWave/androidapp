715d5f05c355b3b4b8cad34f87303b35486389d7
jkruljac <jkruljac@hotmail.com>
2015-10-13 18:09:18 -0700

    Added context to the API class Refactored to response pattern.

--- app/src/main/java/com/litewaveinc/litewave/activities/LiteWave_Splash.java
+++ app/src/main/java/com/litewaveinc/litewave/activities/LiteWave_Splash.java
@@ -1,9 +1,7 @@
 package com.litewaveinc.litewave.activities;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,9 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.litewaveinc.litewave.util.JSONHelper;
 import com.litewaveinc.litewave.R;
-import com.litewaveinc.litewave.util.RESTClientHelper;
 import com.litewaveinc.litewave.services.API;
 import com.litewaveinc.litewave.services.APIResponse;
 
@@ -33,9 +29,54 @@ public class LiteWave_Splash extends AppCompatActivity {
 
     public class EventsResponse extends APIResponse {
 
+        private String getCurrentStadium(JSONArray eventCollection) throws JSONException {
+            String result = "";
+
+            for (int i = 0; i < eventCollection.length(); i++) {
+                JSONObject jsonobject = eventCollection.getJSONObject(i);
+                String serverDate = jsonobject.getString("date");
+                serverDate = serverDate.substring(0, serverDate.indexOf('T'));
+
+                Date currentDate = Calendar.getInstance().getTime();
+                //Format the datetime to match the format so we con compare against the current date
+                java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("y-MM-d");
+                String formattedCurrentDate = simpleDateFormat.format(currentDate);
+
+                //If there is an event today return the stadium id.
+                if (0 == serverDate.compareTo(formattedCurrentDate)) {
+                    result = jsonobject.getString("_stadiumId");
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
         @Override
         public void success(JSONArray content) {
             Log.d("Debug", content.toString());
+
+            String currentStadiumID = "";
+            try {
+                if (content != null &&
+                        (getCurrentStadium(content) != null)) {
+                    currentStadiumID = getCurrentStadium(content);
+
+                    Intent intent = new Intent(LiteWave_Splash.this, ChooseLevel.class);
+                    //Setup a bundle to be passed to the next intent
+                    Bundle b = new Bundle();
+                    //Pass the StadiumID to be used to get the seat information
+                    b.putString("StadiumID", currentStadiumID); //Your id
+                    intent.putExtras(b); //Put your id to your next Intent
+                    startActivity(intent);
+                    finish();
+                } else {
+                    //TOD: Possibly add a new activity for no events or pass to the next choose level activity.
+                    _noEvents.setText(R.string.noEventsToday);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -50,94 +91,15 @@ public class LiteWave_Splash extends AppCompatActivity {
         this._eventsURL = getString(R.string.apiURL) + getString(R.string.getEvents);
 
         this._eventsURL = this._eventsURL.replaceFirst("\\[clientID\\]",
-                                getString(R.string.clientID_Blazers));
+                getString(R.string.clientID_Blazers));
 
-        //Excecutes a Async Task from the main UX thread
-        new checkEvents().execute("");
+        API.getEvents(getApplicationContext(), new EventsResponse());
 
-        API.getEvents(new EventsResponse());
-    }
-
-    private class checkEvents extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            //TODO: do while for future retry logic
-            //do {
-                try {
-                    Thread.sleep(3000);
-                    return RESTClientHelper.callRESTService(_eventsURL);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            //} while (RESTClientHelper.callRESTService(getString(R.string.getEventsURL)) == null);
-            return null;
-        }
-
-        private String getCurrentStadium(JSONArray eventCollection) throws JSONException {
-            String result = "";
-            for(int i = 0 ; i < eventCollection.length(); i++){
-                JSONObject jsonobject = eventCollection.getJSONObject(i);
-                String serverDate = jsonobject.getString("date");
-                serverDate = serverDate.substring(0,serverDate.indexOf('T'));
-
-                Date currentDate = Calendar.getInstance().getTime();
-                //Format the datetime to match the format so we con compare against the current date
-                java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("y-MM-d");
-                String formattedCurrentDate = simpleDateFormat.format(currentDate);
-
-                //If there is an event today return the stadium id.
-                if(0 == serverDate.compareTo(formattedCurrentDate))
-                {
-                    result = jsonobject.getString("_stadiumId");
-                    return result;
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            String currentStadiumID = "";
-            try {
-            if((result != "" && result !="[]" && result != null) &&
-                    (getCurrentStadium(JSONHelper.getJSONArray(result)) != null))
-            {
-                currentStadiumID = getCurrentStadium(JSONHelper.getJSONArray(result));
-
-                Intent intent = new Intent(LiteWave_Splash.this, ChooseLevel.class);
-                //Setup a bundle to be passed to the next intent
-                Bundle b = new Bundle();
-                //Pass the StadiumID to be used to get the seat information
-                b.putString("StadiumID", currentStadiumID); //Your id
-                intent.putExtras(b); //Put your id to your next Intent
-                startActivity(intent);
-                finish();
-            }
-            else
-            {
-                //TOD: Possibly add a new activity for no events or pass to the next choose level activity.
-                _noEvents.setText(R.string.noEventsToday);
-            }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            if(_isDebug)
-            {
-                //TODO: Maybe add debug URLS?
-            }
-            else if(RESTClientHelper.callRESTService(_eventsURL) == null)
-            {
-                _noEvents.setText(R.string.noEventsToday);
-            }
+        try {
+            //Sleeping to show the splash screen.
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
