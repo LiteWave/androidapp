6d31468ff406629be9af1cf52ff863fabdc3f17e
jkruljac <jkruljac@hotmail.com>
2015-11-04 21:45:31 -0800

    Added Image download task. Added initial logic to show winner image

--- app/src/main/java/com/litewaveinc/litewave/activities/ResultsActivity.java
+++ app/src/main/java/com/litewaveinc/litewave/activities/ResultsActivity.java
@@ -4,11 +4,20 @@ import com.litewaveinc.litewave.R;
 import com.litewaveinc.litewave.services.Config;
 import com.litewaveinc.litewave.util.SystemUiHider;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
 
 
 public class ResultsActivity extends AppCompatActivity {
@@ -27,5 +36,44 @@ public class ResultsActivity extends AppCompatActivity {
                 Integer.parseInt(colorRGB[1]),
                 Integer.parseInt(colorRGB[2]));
         actionBar.setBackgroundDrawable(new ColorDrawable(color));
+
+        String winnerUserLocationID = (String)Config.get("WinnerUserLocationID");
+        String winnerURL = (String)Config.get("WinnerURL");
+        String winnerImageURL = (String)Config.get("WinnerImageURL");
+
+        if(!winnerImageURL.isEmpty()) {
+            new DownloadImageTask((ImageView) findViewById(R.id.imageViewWinner))
+                    .execute(winnerImageURL);
+            ImageView winnerView = (ImageView) findViewById(R.id.imageViewWinner);
+            winnerView.setVisibility(View.VISIBLE);
+            TextView thanksView = (TextView) findViewById(R.id.textThanks);
+            thanksView.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
